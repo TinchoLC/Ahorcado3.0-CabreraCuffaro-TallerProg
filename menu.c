cbc23a33436e3f6f1cd1cf943315018703c7d136
Tatoair <101194995+Tatoair@users.noreply.github.com>
2022-04-23 16:47:53 -0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -47,35 +47,28 @@ int palabra_repetida(char **palabras, int cant_palabras, char *palabra){
 
 
 char** elegir_palabra(const char *nombre_archivo){
-  char **palabras_totales = malloc(sizeof(char) * CANT_MAX_PALABRAS);
-
-  char **palabras_opcion = malloc(sizeof(char) * 5);
-
-  int indice_palabra_aleatoria;
+  char **palabras_totales = malloc(sizeof(char*) * CANT_MAX_PALABRAS);
+  char **palabras_opcion = malloc(sizeof(char*) * 5);
 
   FILE *archivo_objeto = abrir_archivo(nombre_archivo, "r");
   int cant_palabras = leer_palabras(archivo_objeto, palabras_totales);
   fclose(archivo_objeto);
-
+  
+  int indice_palabra_aleatoria;
   for (int i = 0; i < 5; i++) {
     indice_palabra_aleatoria = numero_aleatorio(0, cant_palabras);
 
-    if ( palabra_repetida(**palabras_opcion, i , palabras_totales[indice_palabra_aleatoria]) ) {
+    if (palabra_repetida(**palabras_opcion, i + 1, palabras_totales[indice_palabra_aleatoria])) {
       i--; // Como la palabra se repite, hace otra iteracion
-    }
-    else{ // La palabra solamente se guarda si no es repetida
-    int largo_palabra_aleatoria = strlen(palabras_totales[indice_palabra_aleatoria]);
-    palabras_opcion[i] = malloc(sizeof(char) * (largo_palabra_aleatoria + 1));
-    strcpy(palabras_opcion[i], palabras_totales[indice_palabra_aleatoria]);
+    } else { // La palabra solamente se guarda si no es repetida
+      int largo_palabra_aleatoria = strlen(palabras_totales[indice_palabra_aleatoria]);
+      palabras_opcion[i] = malloc(sizeof(char) * (largo_palabra_aleatoria + 1));
+      strcpy(palabras_opcion[i], palabras_totales[indice_palabra_aleatoria]);
     }
   }
   liberar_palabras(palabras_totales, CANT_MAX_PALABRAS);
   return palabras_opcion;
 }
-
-// para agregar funcion
-
-
 
 
 int menu(){
@@ -86,7 +79,7 @@ int menu(){
   switch (opcion) {
   
     case 1: // Elegir una palabra
-    // eleccionPalabra();  /////////////////////////////////////////
+    // elegir_palabra();  /////////////////////////////////////////
   break;
 
   case 2: // Mostrar el historial 
